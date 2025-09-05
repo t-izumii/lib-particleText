baa6b99ac24f75c0545e8a20bf5422e12d6e216e
t-izumii <120708670+t-izumii@users.noreply.github.com>
2025-09-05 20:37:48 +0900

    ビルドパスの修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/lib-particleText/",
   build: {
-    outDir: 'docs'
-  }
-})
+    outDir: "docs",
+  },
+});
